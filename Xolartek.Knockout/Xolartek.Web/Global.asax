9ba36661cdb89b344d3a7d6f8324338b78bcd494
VinceGonzales <vince.gonzales@hotmail.com>
2018-08-15 09:54:58 -0700

    Initial Commit.

---
+++ Xolartek.Knockout/Xolartek.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Xolartek.Web.Global" Language="C#" %>
---
+++ Xolartek.Knockout/Xolartek.Web/Scripts/typescripts/main.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=C:/Users/LADBSCID11/Source/Repos/Knockout-Sandbox/Knockout-Sandbox/Xolartek.Knockout/Xolartek.Web/Scripts/typescripts/main.js.map
