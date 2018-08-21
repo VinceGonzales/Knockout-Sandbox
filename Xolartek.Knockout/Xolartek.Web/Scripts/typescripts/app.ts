52fcd22f7e9942754ff832c1c265fa6f3e2b5357
VinceGonzales <vince.gonzales@hotmail.com>
2018-08-21 13:36:08 -0700

    setup

--- Xolartek.Knockout/Xolartek.Web/Scripts/typescripts/app.ts
+++ Xolartek.Knockout/Xolartek.Web/Scripts/typescripts/app.ts
@@ -1,1 +1,29 @@
-﻿
+﻿(function () {
+    // Button type is Element
+    let button: Element = document.querySelector('.btn');
+    // Input type is HTMLInputElement
+    let input: HTMLInputElement = <HTMLInputElement>document.querySelector('.form-control');
+    // Heading tag is HTMLElement
+    let heading: HTMLElement = document.querySelector('.headline') as HTMLElement;
+    // Tuples allow for varying types
+    let flexibleArray: [string, number, boolean] = ['John Doe', 24, true];
+    // Enumerators
+    enum Status { Started = 33, InProgress = 66, Completed = 100 }
+    let status: Status = Status.InProgress;
+    console.log("The project is ", status, "% completed.");
+    button.addEventListener('click', handleButtonClick);
+    // readonly modifier
+    let settings: ScreenSettings = { width: 1920, height: 1080 };
+    console.log("Display settings: ", settings);
+})()
+
+function handleButtonClick(evtObject: MouseEvent) {
+    evtObject.preventDefault();
+    let btn: Element = <Element>evtObject.target;
+    console.log(btn.innerHTML);
+}
+
+interface ScreenSettings {
+    readonly width: number;
+    readonly height: number;
+}
--- Xolartek.Knockout/Xolartek.Web/Scripts/typescripts/main.js
+++ Xolartek.Knockout/Xolartek.Web/Scripts/typescripts/main.js
@@ -1,1 +1,23 @@
+(function () {
+    var button = document.querySelector('.btn');
+    var input = document.querySelector('.form-control');
+    var heading = document.querySelector('.headline');
+    var flexibleArray = ['John Doe', 24, true];
+    var Status;
+    (function (Status) {
+        Status[Status["Started"] = 33] = "Started";
+        Status[Status["InProgress"] = 66] = "InProgress";
+        Status[Status["Completed"] = 100] = "Completed";
+    })(Status || (Status = {}));
+    var status = Status.InProgress;
+    console.log("The project is ", status, "% completed.");
+    button.addEventListener('click', handleButtonClick);
+    var settings = { width: 1920, height: 1080 };
+    console.log("Display settings: ", settings);
+})();
+function handleButtonClick(evtObject) {
+    evtObject.preventDefault();
+    var btn = evtObject.target;
+    console.log(btn.innerHTML);
+}
 //# sourceMappingURL=C:/Users/LADBSCID11/Source/Repos/Knockout-Sandbox/Knockout-Sandbox/Xolartek.Knockout/Xolartek.Web/Scripts/typescripts/main.js.map
