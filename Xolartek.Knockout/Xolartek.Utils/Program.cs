44a67fc8e62d7215ea3d2cc8a8784ff038d2ad67
VinceGonzales <vince.gonzales@hotmail.com>
2018-10-17 12:33:45 -0700

    revised utility for viewmodel generation

--- Xolartek.Knockout/Xolartek.Utils/Program.cs
+++ Xolartek.Knockout/Xolartek.Utils/Program.cs
@@ -14,24 +14,20 @@ namespace Xolartek.Utils
     /// </summary>
     class Program
     {
+        /// <summary>
+        /// 1st argument should be the absolute path of the dll
+        /// 2nd argument should be the full namespace of the class
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             string assemblyLocation = args[0];
             string className = args[1];
-            // This variable holds the amount of indenting that 
-            // should be used when displaying each line of information.
-            Int32 indent = 0;
-            // Display information about the EXE assembly.
+            
             Assembly b = Assembly.LoadFile(assemblyLocation);
-
-            Display(indent, "Assembly: {0}", b);
-
-            indent += 1;
+            
             Type t = b.GetType(className);
-            ReadInfo(t, indent);
-            indent = 1;
-            t = b.GetType("Xolartek.Web.Models.SkillVM");
-            ReadInfo(t, indent);
+            CreateKnockout(assemblyLocation, t);
             Console.ReadLine();
         }
         private static void CreateVM(string assemblyLocation, string className, string jsfilename)
@@ -64,51 +60,118 @@ namespace Xolartek.Utils
             tw = null;
         }
 
-        private static void ReadInfo(Type t, Int32 indent)
+        private static void CreateKnockout(string assemblyLocation, Type t)
         {
-            Display(indent, "Type: {0}", t);
-            // For each type, show its members & their custom attributes.
-            indent += 1;
+            System.IO.TextWriter tw = new StreamWriter(Environment.GetEnvironmentVariable("USERPROFILE") + @"\Desktop\" + t.Name + ".js");
+            List<string> properties = new List<string>();
+            tw.Write("function {0}(", t.Name);
+            foreach (PropertyInfo prop in t.GetProperties())
+            {
+                properties.Add(prop.Name);
+            }
+            tw.Write(string.Join(", ", properties.ToArray()).ToLower());
+            tw.Write(") {" + System.Environment.NewLine);
+
+            // itierate through its members
             foreach (MemberInfo mi in t.GetMembers())
             {
+                // make sure it is not a custom attribute
                 if (mi.GetCustomAttributes(false).Length == 0)
                 {
-                    // If the member is a method, display information about its parameters.
+                    // If the member is a method...
                     if (mi.MemberType == MemberTypes.Method)
                     {
-                        Display(indent, "Method: {0}", mi.Name);
-
+                        List<string> props = new List<string>();
+                        tw.Write("\tthis.{0} = function(", mi.Name);
                         foreach (ParameterInfo pi in ((MethodInfo)mi).GetParameters())
                         {
-                            Display(indent + 1, "Parameter: Type={0}, Name={1}", pi.ParameterType, pi.Name);
+                            props.Add(pi.Name);
                         }
+                        tw.Write(string.Join(", ", props.ToArray()).ToLower());
+                        tw.Write(") {" + System.Environment.NewLine);
+                        // write code
+                        tw.Write("}" + System.Environment.NewLine);
                     }
 
-                    // If the member is a property, display information about the property's accessor methods.
+                    // If the member is a property...
                     if (mi.MemberType == MemberTypes.Property)
                     {
-                        Display(indent, "Property: {0}", mi.Name);
                         PropertyInfo info = mi as PropertyInfo;
                         string propType = info.PropertyType.Name;
+                        
                         if (propType.IndexOf("List") != -1)
                         {
-                            Type genericListType = info.PropertyType.GetGenericArguments()[0];
-                            Display(indent + 1, "List type: {0}", genericListType.FullName);
+                            tw.WriteLine("\tthis.{0} = ko.observableArray({1});", mi.Name, mi.Name.ToLower());
                         }
                         else
                         {
-                            Display(indent + 1, "Property type: {0}", propType);
+                            tw.WriteLine("\tthis.{0} = ko.observable({1});", mi.Name, mi.Name.ToLower());
+                        }
+
+                    }
+                }
+            }
+
+            tw.WriteLine("}" + System.Environment.NewLine);
+
+            tw.Flush();
+            tw.Close();
+            tw = null;
+        }
+        private static void CreateTypeScript(string assemblyLocation, Type t)
+        {
+            // itierate through its members
+            foreach (MemberInfo mi in t.GetMembers())
+            {
+                // make sure it is not a custom attribute
+                if (mi.GetCustomAttributes(false).Length == 0)
+                {
+                    // If the member is a method...
+                    if (mi.MemberType == MemberTypes.Method)
+                    {
+                        Console.WriteLine("Method {0}", mi.Name);
+                        foreach (ParameterInfo pi in ((MethodInfo)mi).GetParameters())
+                        {
+                            Console.WriteLine("Parameter {0} : {1}", pi.Name, pi.ParameterType);
+                        }
+                    }
+
+                    // If the member is a property...
+                    if (mi.MemberType == MemberTypes.Property)
+                    {
+                        PropertyInfo info = mi as PropertyInfo;
+                        string propType = info.PropertyType.Name;
+
+                        if (propType.IndexOf("List") != -1)
+                        {
+                            Type genericListType = info.PropertyType.GetGenericArguments()[0];
+                            Console.WriteLine("List {0} : {1}", mi.Name, genericListType.FullName);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Property {0} : {1}", mi.Name, propType);
                         }
 
                     }
                 }
             }
         }
-        // Display a formatted string indented by the specified amount.
-        private static void Display(Int32 indent, string format, params object[] param)
+        private static string JsType(string clrType)
         {
-            Console.Write(new string(' ', indent * 2));
-            Console.WriteLine(format, param);
+            string result = "any";
+            switch(clrType)
+            {
+                case "Int32":
+                    result = "number";
+                    break;
+                case "String":
+                    result = "string";
+                    break;
+                case "Boolean":
+                    result = "boolean";
+                    break;
+            }
+            return result;
         }
     }
 }
